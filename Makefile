d78726c9318639f96173f1236cbd9e62a81b7a2f
Hicham Fhad <hfhad@e2r6p13.1337.ma>
2026-06-15 11:41:17 +0100

    push

---
+++ Makefile
@@ -0,0 +1,23 @@
+COMPOSE_FILE = srcs/docker-compose.yml
+DATA_PATH = /home/$(USER)/data
+
+all:
+	@mkdir -p $(DATA_PATH)/mariadb
+	@mkdir -p $(DATA_PATH)/wordpress
+	@docker compose -f $(COMPOSE_FILE) up --build -d
+
+down:
+	docker compose -f $(COMPOSE_FILE) down
+
+stop:
+	docker compose -f $(COMPOSE_FILE) stop
+
+clean: down
+	docker compose -f $(COMPOSE_FILE) down -v
+
+fclean: clean
+	docker system prune -af
+
+re: fclean all
+
+.PHONY: all down sop clean fclean re
---
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+MYSQL_PASSWORD=$(cat /run/secrets/db_password)
+MYSQL_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
+
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    mariadb-install-db --user=mysql --datadir=/var/lib/mysql
+    mariadbd --user=mysql --skip-networking &
+    sleep 5
+
+    while ! mariadb -u root -e "SELECT 1" > /dev/null 2>&1; do
+        echo "Waiting for Mariadb to be ready..."
+        sleep 1
+    done
+    mariadb -u root <<EOF
+CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+FLUSH PRIVILEGES;
+EOF
+    mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
+fi
+echo "Starting MariaDB..."
+exec mariadbd --user=mysql
