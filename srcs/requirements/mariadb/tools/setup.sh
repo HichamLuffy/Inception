804178a1a859725d636172c0c83205108f01957d
HichamLuffy <imalexvssteve2020@gmail.com>
2026-07-07 11:11:12 -0400

    push

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -3,15 +3,11 @@
 MYSQL_PASSWORD=$(cat /run/secrets/db_password)
 MYSQL_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
 
-if [ ! -d "/var/lib/mysql/.initialized" ]; then
+if [ ! -f "/var/lib/mysql/.initialized" ]; then
     mariadb-install-db --user=mysql --datadir=/var/lib/mysql
     mariadbd --user=mysql --skip-networking &
     sleep 5
 
-    while ! mariadb -u root -e "SELECT 1" > /dev/null 2>&1; do
-        echo "Waiting for Mariadb to be ready..."
-        sleep 1
-    done
     mariadb -u root <<EOF
 CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
 CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
